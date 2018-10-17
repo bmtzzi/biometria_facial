258e5c57d6caf55088376504b7d8462b1cf230cf
brunoMTrazzi <bruno.trazzi@usp.br>
2018-10-17 18:28:56 -0300

    Ajudam na inicialização do programa compilado com configurações pré definidas

---
+++ alt2_execFace_classify.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./biometriaFacial ../cascade_classifiers/haarcascade_frontalface_alt2.xml ./classifiers_training_photos/fotos.csv 0
---
+++ default_execFace_classify.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./biometriaFacial ../cascade_classifiers/haarcascade_frontalface_default.xml ./classifiers_training_photos/fotos.csv 0
