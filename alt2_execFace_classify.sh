b1732e60debb17645c49701be6c526d2a57475eb
brunoMTrazzi <bruno.trazzi@usp.br>
2018-11-03 22:08:32 -0300

    Corrigi o caminho para os classificadores

--- alt2_execFace_classify.sh
+++ alt2_execFace_classify.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./biometriaFacial ../cascade_classifiers/haarcascade_frontalface_alt2.xml ./classifiers_training_photos/fotos.csv 0
+./biometriaFacial haar_cascade/haarcascade_frontalface_alt2.xml ./classifiers_training_photos/fotos.csv 0
